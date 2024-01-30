0c792274e7d1bda483c330ac526b1f435f230482
Ankitneo <gupta.ankit@neosoftmail.com>
2024-01-30 18:17:59 +0530

    done changes

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ require 'rails/test_unit/railtie'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module BlogApp
+module ApiTestApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
